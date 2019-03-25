4722e33b05383519850cc3d09ff64b79cf2b9e3c
Andrea Titton <andrea.titton.ext@accurat.it>
2019-03-25 14:20:23 +0100

    🎧 modulated audio

--- src/play.py
+++ src/play.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
 from pyo import midiToHz, Choice, Port, Sine, SuperSaw, FM, Delay, STRev  # pylint: disable=E0611
@@ -5,17 +6,20 @@ from pyo import midiToHz, Choice, Port, Sine, SuperSaw, FM, Delay, STRev  # pyli
 from load_data import fetch_data_iter
 from pyo_server import server as s
 
-pitches = [midiToHz(np.mean(m)) for m in fetch_data_iter]
+means = [np.mean(m) for m in fetch_data_iter]
+range_ = [(i - np.min(means))/np.ptp(means) for i in means]
+means = [(i * 100) for i in range_]
+
+pitches = [midiToHz(m) for m in means]
 
 # Add more voices here to generate a simple counterpoint
 choice = Choice(choice=pitches, freq=1)
-ch_port = Port(choice, risetime=.001, falltime=.001)
 
 # Two simple instruments
 lfdetune = Sine(freq=0.1, mul=.07, add=.07)
-instrument1 = SuperSaw(freq=ch_port, detune=lfdetune, mul=.1)
-lfind = Sine(freq=0.1, phase=0.5, mul=3, add=3)
-instrument2 = FM(carrier=ch_port, ratio=1.0025, index=lfind, mul=.025)
+instrument1 = SuperSaw(freq=choice, detune=lfdetune, mul=.8)
+lfind = Sine(freq=0.5, phase=0.5, mul=3, add=3)
+instrument2 = FM(carrier=choice, ratio=1.0025, index=lfind, mul=.8)
 
 # Send instruments output to delay
 src_sum = instrument1.mix(2) + instrument2.mix(2)
@@ -24,6 +28,7 @@ comb = Delay(src_sum, delay=lfdel, feedback=.5)
 
 # Send two resulting signals to reverb and output
 out_sum = src_sum + comb
-rev = STRev(out_sum, cutoff=3500, bal=.5, roomSize=2).out()
+rev = out_sum.out()
 
+s.start()
 s.gui(locals())
